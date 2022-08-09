f36a2d458ff8880c6fd897212a4992a795730a37
Kirill <sonik3000.83@gmail.com>
2022-08-09 16:03:07 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Post post = new Post();
+        post.name = "Андрей";
+        post.passport = "9205 № 444555";
+        post.patronymic = "Андреевич";
+        post.phone = "+7(974)-444-55-66";
+        post.surname = "Андреев";
+        post.subscription = "true";
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 10;
+        post.birthday.year = 1983;
+
+
+    }
+
+
+}
